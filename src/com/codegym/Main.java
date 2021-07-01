0148a84cd93b09f4fd41c3d80ccc49ed9ecf3954
thaophamhetxat <thaophamhetxat@gmail.com>
2021-07-01 13:34:17 +0700

    crawl tin tuc

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,36 @@
+package com.codegym;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            URL url = new URL("http://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            scanner.close();
+
+            content = content.replaceAll("\\n+", "");
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()){
+                System.out.println(m.group(1));
+            }
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
